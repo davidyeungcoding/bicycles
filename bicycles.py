06760ef71b6f377a95082880d0944eba16c0c29c
yeungpirate <yeungpirate@gmail.com>
2018-02-23 23:24:21 +0000

    Something's not right

--- bicycles.py
+++ bicycles.py
@@ -15,6 +15,9 @@ class Customer(object):
     def __init__(self, name, cash):
         self.name = name
         self.cash = cash
+        
+    def buy(self, bike_model, bike_shop):
+        bike_shop.sell(bike_model)
 
 class BikeShop(object):
     
@@ -28,9 +31,11 @@ class BikeShop(object):
     #     for bikes, stock in supply.items():
     #         print(bikes.bike(), ':', stock)
             
-    def sell(self, profit):
-        """Adds sold bikes to the sold_bikes list"""
-        self.profit = 0.2 * cost
+    def sell(self, bike_model):
+        """Subtracts stock and calculates profit"""
+        bike, stock = shop_inventory[bike_model]
+        self.shop_inventory[bike_model] = (bike, stock - 1)
+        self.profit += 0.2 * bike.cost
 
 if __name__ == "__main__":
     bike_1 = Bicycle('Schwinn', 'Protocol 1.0', '41 lbs.', 320.0)
@@ -44,15 +49,17 @@ if __name__ == "__main__":
     person_2 = Customer('Jemma Cain', 500.00)
     person_3 = Customer('Marco Reus', 1000.00)
     
-    shop_inventory = {
-        'Protocol 1.0': (bike_1, 5),
-        'Ladies Perla': (bike_2, 5),
-        'Palmentto': (bike_3, 5),
-        'XC700': (bike_4, 5),
-        'Marina': (bike_5, 5),
-        'Fresno': (bike_6, 5),
-    }
+    shop_inventory = [
+        'Protocol 1.0', (bike_1, 5),
+        'Ladies Perla', (bike_2, 5),
+        'Palmentto', (bike_3, 5),
+        'XC700', (bike_4, 5),
+        'Marina', (bike_5, 5),
+        'Fresno', (bike_6, 5),
+    ]
     
     shop = BikeShop('Cycles', shop_inventory)
     
+    person_1.buy('Protocol 1.0', shop)
     print(shop.inventory)
+    print(shop.profit)
